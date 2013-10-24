fb1ba7f071d5606233d429c372f8840bbe342fe2
Simon Proctor <simon.proctor@photobox.com>
2013-10-24 08:30:53 +0100

    End of 2013-10-23

--- js/comp-loader.js
+++ js/comp-loader.js
@@ -1,42 +1,55 @@
-var exploder = {
-	breakpoint : 600,
-	explode : function() {
-		if ( window.self == window.top ) {
-			$('.explode').each( function() {
-				var ex = !! ( $('body').width() > exploder.breakpoint );
-				if ( ex ) {
-					$('body').addClass('exploded');
-				} else {
-					$('body').removeClass('exploded');
+var comp_loader = {
+	breakpoints : {},
+	init : function() {
+		// Register all embed links 
+		$('.embed').each( comp_loader.register );
+	},
+	register : function() {
+		var url = $(this).attr( 'href' );
+		// Id is the file name less last extension
+		var comp_id = url.split( "/" ).pop().replace( /\..+$/, "" );
+		// Embed target is the element with and id "embed-target-" + id OR the immediate parent
+		var $embed_target = $(this).parents("#embed-target-" + comp_id); 
+		if ( ! $embed_target.length ) { $embed_target = $(this).parent(); }
+		if ( ! comp_loader.breakpoints[comp_id] ) {
+			$.ajax( url, { success : comp_loader.load( url, comp_id, $embed_target ) } );
+			comp_loader.breakpoints[comp_id] = {};
+		}
+	},
+	load : function( url, comp_id, $embed_target ) {
+		//console.log( comp_id );
+		return function( data, textStatus, jqXHR ) {
+			var nodes = $.parseHTML( data );
+			// Look for stylsheets to embed and the 
+			_.each( nodes, function( node ) {
+				$node = $(node);
+				if ( $node.filter( 'link.embeded' ).length ) {
+					comp_loader.embed_style( $node );
 				}
-
-				var url = $(this).attr( 'href' ) ? $(this).attr( 'href' ) : $(this).attr( 'data' );
-				var klass = $(this).attr( 'class' );
-				var text = $(this).data('text') ? $(this).data('text') : $(this).html();
-				if ( ex ) {
-					$(this).replaceWith( '<object data-text="'+ text +'" class="'+ klass +'" data="'+ url +'" type="text/html"></object>' );
-				} else {
-					$(this).replaceWith( '<a class="'+ klass +'" href="'+ url +'">'+ text +'</a>' );
+				if ( $node.filter( '#comp-' + comp_id ).length ) {
+					comp_loader.get_data( $node , $embed_target );
+				}
+				if ( $node.find( '#comp-' + comp_id ).length ) {
+					comp_loader.get_data( $node.find( '#comp-' + comp_id ) , $embed_target );
 				}
 			});
-		} else {
-			$('.explode').remove();
-			$('body').addClass('child');
+			comp_loader.init();
+//			console.log( $( doc ).find( 'link.embed' ) );
 		}
 	},
-	checkExplode : function() {
-		var ex = !! ( $('body').width() > exploder.breakpoint );
-		if ( ex != $('body').hasClass('exploded' ) ) {
-			exploder.explode();
+	embed_style : function( $node ) {
+		var url = $node.attr('href');
+		if ( $('link[href="'+url+'"]').length == 0 ) {
+			$('head').append( $node );
 		}
 	},
-	init : function() {
-		exploder.explode();
-		if ( window.self == window.top ) {
-			$(window).on('resize', exploder.checkExplode );
-		}
+	get_data : function( $node, $embed_target ) {
+		var original = $embed_target.html();
+		//console.log( original );
+		$embed_target.html('').append( $node ).data('original', original );
 	}
+	
 }
 
-$().ready( exploder.init );
+$().ready( comp_loader.init );
 
